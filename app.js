bf3e629c31c1420d4a82f1e5b001db154acd2526
duandun <duandun@gmail.com>
2015-06-01 00:56:20 +0800

    mongodb schema和model创建

--- app.js
+++ app.js
@@ -9,8 +9,12 @@ var bodyParser = require('body-parser');
 // 	res.render('index', {title: 'imooc'});
 // });
 var path = require('path');
+var mongoose = require('mongoose');
 var port = process.env.PORT || 3000;
 var app = express();
+
+var Movie = require('./models/movie');
+mongoose.connect('mongodb://localhost/imooc');
 
 app.set('views', './views/pages');
 app.set('view engine', 'jade');
@@ -21,51 +25,27 @@ console.log('imooc started on port' + port);
 
 // index page
 app.get('/', function(req, res) {
-	res.render('index', {
-		title: 'imooc 首页',
-		movies: [{
-			title: '机械战警',
-			_id: 1,
-			poster: ''
-		},
-		{
-			title: '机械战警',
-			_id: 2,
-			poster: ''
-		},
-		{
-			title: '机械战警',
-			_id: 3,
-			poster: ''
-		},
-		{
-			title: '机械战警',
-			_id: 4,
-			poster: ''
-		},
-		{
-			title: '机械战警',
-			_id: 5,
-			poster: ''
-		},
-		]
+	Movie.fetch(function(err, movies) {
+		if(err) {
+			console.log(err);
+		}
+		res.render('index', {
+			title: 'imooc 首页',
+			movies: movies
 	});
+	})
+	
 });
 // detail page
 app.get('/movie/:id', function(req, res) {
-	res.render('detail', {
-		title: 'imooc 详情页',
-		movie: {
-			doctor: '何塞',
-			country: '美国',
-			title: '机械战警',
-			year: 2014,
-			poster: '',
-			language: '英语',
-			flash: '',
-			summary: '哈哈哈 啦啦啦，大大。'
-		}
+	var id = req.params.id;
+	Movie.findById(id, function(err, movie) {
+		res.render('detail', {
+			title: 'imooc ' + movie.title,
+			movie: movie
 	});
+	})
+
 });
 
 //list page
@@ -88,7 +68,17 @@ app.get('/admin/list', function(req, res) {
 
 // admin page
 app.get('/admin/movie', function(req, res) {
-	res.render('index', {
-		title: 'imooc 后台录入页面'
+	res.render('admin', {
+		title: 'imooc 后台录入页面',
+		movie: {
+			doctor: '',
+			country: '',
+			title: '',
+			year: '',
+			poster: '',
+			language: '',
+			flash: '',
+			summary: ''
+		}
 	});
 });
